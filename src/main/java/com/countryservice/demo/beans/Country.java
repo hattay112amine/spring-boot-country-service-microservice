767a53ea6dd9acf86f6efa28eee0ae6db5ac81ef
hattay112amine <hattay112@gmail.com>
2025-10-16 19:07:01 +0100

    Initial commit of Spring Boot Country Service Microservice

---
+++ src/main/java/com/countryservice/demo/beans/Country.java
@@ -0,0 +1,47 @@
+package com.countryservice.demo.beans;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Table(name="country")
+public class Country {
+	
+	@Id
+	@Column(name="id")
+	private int idCountry;
+	@Column(name="name_country")
+	private String name;
+	@Column(name="capital_name")
+	private String Capital;
+	 public Country(int id, String name, String capital) {
+	        this.idCountry = id;
+	        this.name = name;
+	        this.Capital = capital;
+	    }
+	public int getIdCountry() {
+		return idCountry;
+	}
+	public void setIdCountry(int idCountry) {
+		this.idCountry = idCountry;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCapital() {
+		return Capital;
+	}
+	public void setCapital(String capital) {
+		Capital = capital;
+	}
+
+}
---
+++ src/test/java/com/countryservicedemo/demo/CountryServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.countryservicedemo.demo;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes= {CountryServiceApplicationTests.class})
+class CountryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
